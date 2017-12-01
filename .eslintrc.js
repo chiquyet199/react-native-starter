759398fb5bc1a8f445511c811e43026cdd88d6a9
Quyết Nguyễn <quyet.nguyen@aleph-labs.com>
2017-12-01 17:25:03 +0700

    turn on check prop-types

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
 	extends: 'airbnb',
 	plugins: ['react', 'jsx-a11y', 'import'],
+	parser: 'babel-eslint',
 	settings: {
 		'import/resolver': {
 			'babel-module': {},
@@ -12,10 +13,10 @@ module.exports = {
 		'no-tabs': 0,
 		'no-mixed-spaces-and-tabs': 0,
 		'react/jsx-filename-extension': 0,
-		'react/prop-types': 0,
 		'react/jsx-indent': [2, 'tab'],
 		'react/jsx-indent-props': [2, 'tab'],
 		'react/prefer-stateless-function': 0,
+		'react/no-typos': 0,
 		'no-unused-expressions': 0,
 	},
 };
--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
+import PropTypes from 'prop-types';
 
 import styles from './styles';
 
 class Container extends Component {
+	static propTypes = {
+		center: PropTypes.bool,
+		margin: PropTypes.bool,
+		padding: PropTypes.bool,
+		children: PropTypes.node.isRequired,
+	};
+
+	static defaultProps = {
+		center: false,
+		margin: false,
+		padding: false,
+	};
+
 	render() {
 		const { center, margin, padding } = this.props;
 		const containerStyles = [styles.container];
