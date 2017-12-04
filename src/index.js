8f4c11cb5bb511f9d61ceb2d63b7f75a7b50399c
Quyết Nguyễn <quyet.nguyen@aleph-labs.com>
2017-12-04 08:22:49 +0700

    Combine reducer Config store Apply store to our app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
 import EStyleSheet from 'react-native-extended-stylesheet';
 
+import store from 'configs/store';
 import { Root } from 'configs/routes';
 import { commonStyle } from 'configs/common-style-variables';
 
@@ -8,7 +10,11 @@ EStyleSheet.build(commonStyle);
 
 class App extends Component {
 	render() {
-		return <Root />;
+		return (
+			<Provider store={store}>
+				<Root />
+			</Provider>
+		);
 	}
 }
 
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import common from './common';
+import data from './data';
+
+export default combineReducers({
+	common,
+	data,
+});
